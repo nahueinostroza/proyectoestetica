e5d0441ad6356be0a74ca238eee6b4f51ff7c52b
nahuelinostroza <nahuelon8@hotmail.com>
2022-07-07 12:51:15 -0300

    hice cambios en appjs y contacto js

--- app.js
+++ app.js
@@ -19,80 +19,45 @@
 
     if (tratamiento === "facial") {
         console.log("aplicador de micropuncion")
-        precio=4000
     }
 
     if (tratamiento ==="facial"){
         console.log("radiofrecuencia")
-        precio=2000
          }
 
     if (tratamiento === "facial"){
         console.log("mesoterapia virtual")
-        precio=2300
     }
 
     if (tratamiento === "facial"){
         console.log("alta frecuencia")
-        precio=2300
     }
 
     if (tratamiento === "facial"){
         console.log("microdermoabrasion")
-        precio=2000
     }
 
     if (tratamiento === "masajes"){
         console.log("masaje sueco")
-        precio=1800
     }
 
     if (tratamiento === "masajes"){
         console.log("drenaje linfatico manual")
-        precio=2300
     }
 
      if (tratamiento === "masajes"){
         console.log("shiatsu")
-        precio=2300
     }
 
      if (tratamiento === "masajes"){
         console.log("masaje reflejos")
-        precio=2500
     }
 
      if (tratamiento === "masajes"){
         console.log("masaje deportivo")
-        precio=2300
     }
 
-    switch (precio){
-        case 3000:
-            alertaefectivo ()
-            break;
-        case 2000:
-            alertaefectivo ()
-            break;
-        case 2500:
-            alertaefectivo ()
-        case 2300:
-            alertaefectivo ()
-        case 1800:
-            alertaefectivo ()
-        case 4000:
-            alertadebito ()            
-             }
-       
-
-            
-             function alertaefectivo () {
-                console.log("Se tendra que pagar en efectivo")
-            }
-
-            function alertadebito () {
-                console.log("Se puede pagar en efectivo o tarjeta de debito")
-            }
+          
 
     
 
--- contacto.js
+++ contacto.js
@@ -1,22 +1,33 @@
-let formulario = document.getElementById("formulario")
-let enviar = document.getElementById("enviar")
-let borrar = document.getElementById("borrar")
+let elformulario = document.getElementById("formulario");
 
-formulario.addEventListener("submit",(e) => {
-  e.preventDefault()
+elformulario.addEventListener("submit", validarformulario);
 
-  verificarformulario()
+function validarformulario(e) {
+  e.preventDefault();
 
-  const content = document.createElement("div")
-
+  console.log(document.getElementById("email1").value);
+  console.log(document.getElementById("textarea1").value);
   
-  content.innerHTML = `
-  <img src="https://bloggerprise.net/wp-content/uploads/2015/08/Gracias-por-Contactarnos.png"/>
-   `;
-})
-
-function verificarformulario(e){
-  
-  console.log(formulario.children[0].value);
-  console.log(formulario.children[1].value);
 }
+
+let boton =
+document.getElementById("enviar")
+boton.addEventListener("click", formenvi)
+
+function formenvi () {
+  alert ("formulario enviado");
+}
+
+let eliminar =
+document.getElementById("borrar")
+eliminar.onclick = () => elformulario.reset()
+
+
+
+
+
+
+
+
+
+
