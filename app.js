f2ed3be3c07a51346c8c1f3ed16225a5b72367a1
nahuelinostroza <nahuelon8@hotmail.com>
2022-08-01 19:13:59 -0300

    agregue fetch, botones interactivos en la parte de precios y evento en la parte de contacto

--- app.js
+++ app.js
@@ -1,3 +1,59 @@
+let URL=`precios.json`
+let URLma=`masajes.json`
+
+const botonmasajes = document.querySelector("#masaje")
+const botonfacial = document.querySelector("#facial")
+
+botonmasajes.addEventListener ("click", ()=>obtenerContenido(URLma))
+botonfacial.addEventListener ("click", ()=>obtenerPrecio(URL))
+
+
+const obtenerContenido = (URL)=> {
+    let preciosAmostrar = ""
+    fetch(URL)
+   .then((response)=> response.json())
+   .then( (data)=>{
+        for (contenido of data) {
+            preciosAmostrar += listarprecios(contenido)
+        }
+        document.querySelector("#tablaprecios").innerHTML = preciosAmostrar
+   }) 
+}
+
+const listarprecios= (contenido)=> {
+    const {nombre, precio, duracion} = contenido
+    return    `<tr>
+                   <td>${nombre}</td>
+                   <td>${precio}</td>
+                   <td>${duracion}</td>
+               </tr>`
+            }
+
+
+const obtenerPrecio = (URLma)=> {
+    let preciosAmostrar = ""
+    fetch(URLma)
+   .then((response)=> response.json())
+   .then( (data)=>{
+        for (contenido of data) {
+            preciosAmostrar += listadeprecios(contenido)
+        }
+        document.querySelector("#tablaprecios").innerHTML = preciosAmostrar
+   }) 
+}
+
+const listadeprecios= (contenido)=> {
+    const {nombre, precio, duracion} = contenido
+    return    `<tr>
+                   <td>${nombre}</td>
+                   <td>${precio}</td>
+                   <td>${duracion}</td>
+               </tr>`
+            }
+
+
+
+     
      class trat {
         constructor (nombre, precio) {
             this.nombre = nombre;
@@ -5,68 +61,23 @@
         }
     }
     
-    let tratamiento = prompt("¿Qué tratamiento queres hacerte? facial o masajes");
+
+    let tratamientos = []
+
+    let tratamiento1= new trat ("facial");
+    let tratamiento2= new trat ("masajes");
+
+    tratamientos.push (tratamiento1)
+    tratamientos.push (tratamiento2)
+
+
     
     
-    let URL=`precios.json`
-
-
-    const obtenerContenido = (URL)=> {
-        let preciosAmostrar = ""
-        fetch(URL)
-       .then((response)=> response.json())
-       .then( (data)=>{
-            for (contenido of data) {
-                preciosAmostrar += listarprecios(contenido)
-            }
-            document.querySelector("#tablaprecios").innerHTML = preciosAmostrar
-       }) 
-    }
     
-    const listarprecios= (contenido)=> {
-        const {nombre, precio, duracion} = contenido
-        return    `<tr>
-                       <td>${nombre}</td>
-                       <td>${precio}</td>
-                       <td>${duracion}</td>
-                   </tr>`
-                }
-    
-obtenerContenido(URL)
-
-let URLma=`masajes.json`
 
 
-    const obtenerPrecio = (URLma)=> {
-        let preciosAmostrar = ""
-        fetch(URLma)
-       .then((response)=> response.json())
-       .then( (data)=>{
-            for (contenido of data) {
-                preciosAmostrar += listadeprecios(contenido)
-            }
-            document.querySelector("#tablaprecios").innerHTML = preciosAmostrar
-       }) 
-    }
-    
-    const listadeprecios= (contenido)=> {
-        const {nombre, precio, duracion} = contenido
-        return    `<tr>
-                       <td>${nombre}</td>
-                       <td>${precio}</td>
-                       <td>${duracion}</td>
-                   </tr>`
-                }
-    
-obtenerContenido(URLma)
 
-if(tratamiento === facial){
-    console.log("URL");
-}
 
-else if(tratamiento === masajes){
-    console.log("URLma");
-}
 
 
 
